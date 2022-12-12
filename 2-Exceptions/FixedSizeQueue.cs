60e58b96e4921f3b8006fee88f08995d72729659
Giulia Bonifazi - giulia.bonifazi@studio.unibo.it <giulia.bonifazi@studio.unibo.it>
2022-12-12 12:11:53 +0100

    es 2 finito

--- 2-Exceptions/FixedSizeQueue.cs
+++ 2-Exceptions/FixedSizeQueue.cs
@@ -19,17 +19,29 @@ namespace Exceptions
 
         public object GetFirst()
         {
-            // TODO ensure objects can only be retrieve if the item is queue is not empty
-            var first = _items[_firstIndex % Capacity];
-            _firstIndex++;
-            return first;
+            if(_lastIndex != 0)
+            {
+                var first = _items[_firstIndex % Capacity];
+                _firstIndex++;
+                return first;
+            }
+            else
+            {
+                throw new EmptyQueueException("The queue is empty!");
+            }
         }
         
         public void AddLast(object item)
         {
-            // TODO ensure objects can only be inserted if the item is queue is not full
-            _items[_lastIndex % Capacity] = item;
-            _lastIndex++;
+            if(Count < Capacity)
+            {
+                _items[_lastIndex % Capacity] = item;
+                _lastIndex++;
+            }
+            else
+            {
+                throw new FullQueueException("The queue is full!");
+            }
         }
     }
 }
