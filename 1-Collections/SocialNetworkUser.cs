937a4e0deb4bf1cdbd562f778db1d0a2a6e0729d
Giulia Bonifazi - giulia.bonifazi@studio.unibo.it <giulia.bonifazi@studio.unibo.it>
2022-12-12 11:54:44 +0100

    es 1 finito

--- 1-Collections/SocialNetworkUser.cs
+++ 1-Collections/SocialNetworkUser.cs
@@ -1,32 +1,62 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Collections
 {
     public class SocialNetworkUser<TUser> : User, ISocialNetworkUser<TUser>
         where TUser : IUser
     {
+        private IDictionary<string, ISet<TUser>> _followed = new Dictionary<string, ISet<TUser>>();
+
         public SocialNetworkUser(string fullName, string username, uint? age) : base(fullName, username, age)
         {
-            throw new NotImplementedException("TODO is there anything to do here?");
+            
         }
 
         public bool AddFollowedUser(string group, TUser user)
         {
-            throw new NotImplementedException("TODO add user to the provided group. Return false if the user was already in the group");
+            if(_followed.ContainsKey(group) && _followed[group].Contains(user))
+            {
+                return false;
+            }
+            else
+            {
+                if(!(_followed.ContainsKey(group)))
+                {
+                    _followed.Add(group, new HashSet<TUser>());
+                }
+                _followed[group].Add(user);
+                return true;
+            }
         }
 
         public IList<TUser> FollowedUsers
         {
-            get
+            get 
             {
-                throw new NotImplementedException("TODO construct and return the list of all users followed by the current users, in all groups");
+                IList<TUser> AllFollowed = new List<TUser>();
+                foreach (var group in _followed.Values)
+                {
+                    foreach (var user in group)
+                    {
+                        AllFollowed.Add(user);
+                    }
+                }
+                return AllFollowed;
             }
         }
 
         public ICollection<TUser> GetFollowedUsersInGroup(string group)
         {
-            throw new NotImplementedException("TODO construct and return a collection containing of all users followed by the current users, in group");
+            if(_followed.ContainsKey(group))
+            {
+                return _followed[group];
+            }
+            else
+            {
+                return new HashSet<TUser>();
+            }
         }
     }
 }
--- 1-Collections/User.cs
+++ 1-Collections/User.cs
@@ -4,10 +4,21 @@ namespace Collections
 {
     public class User : IUser
     {
+        private readonly string _fullName;
+        private readonly string _username;
+        private readonly uint? _age;
         public User(string fullName, string username, uint? age)
         {
-            throw new NotImplementedException("TODO ensure that username is not null");
-            throw new NotImplementedException("TODO initialise this instance of user accordingly");
+            if( username != null)
+            {
+                _fullName = fullName;
+                _username = username;
+                _age = age;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
         }
         
         public uint? Age { get; }
@@ -16,8 +27,23 @@ namespace Collections
         
         public string Username { get; }
 
-        public bool IsAgeDefined => throw new NotImplementedException("TODO check whether age is non-null or not");
-        
-        // TODO implement missing methods (try to autonomously figure out which are the necessary methods)
+        public bool IsAgeDefined => _age != null;
+
+        public override bool Equals(object obj)
+        {
+            return obj is User user &&
+                   _fullName == user._fullName &&
+                   _username == user._username &&
+                   _age == user._age &&
+                   Age == user.Age &&
+                   FullName == user.FullName &&
+                   Username == user.Username &&
+                   IsAgeDefined == user.IsAgeDefined;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(_fullName, _username, _age, Age, FullName, Username, IsAgeDefined);
+        }
     }
 }
